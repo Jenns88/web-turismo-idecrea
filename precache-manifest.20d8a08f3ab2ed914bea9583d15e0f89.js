0d91e9c20aa08b6259a6cbb5de9c5e1f63c1c48a
Jennifer <jenny_kira@hotmail.com>
2020-03-26 12:44:25 +0100

    First commit

---
+++ precache-manifest.20d8a08f3ab2ed914bea9583d15e0f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806bd0fc20e54fd2929b",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/css/app.ae058bd9.css"
+  },
+  {
+    "revision": "c281fb963be538415890fe27d56a3dea",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/index.html"
+  },
+  {
+    "revision": "806bd0fc20e54fd2929b",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/js/app.0ac5db35.js"
+  },
+  {
+    "revision": "dcefc19107ec80f8ea33",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/js/chunk-vendors.d0a63c46.js"
+  },
+  {
+    "revision": "ca9313a580fde5a0fcfea5b3f3107554",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://jenns88.github.io/web-turismo-idecrea/robots.txt"
+  }
+]);
